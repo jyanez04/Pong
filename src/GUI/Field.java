fa723eadfa980f08ddeff9d3321d2b2c810d5b26
Jake Yanez <jake.yanez589@gmail.com>
2024-08-26 20:35:49 -0600

    Initialize

---
+++ src/GUI/Field.java
@@ -0,0 +1,15 @@
+package GUI;
+
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+
+public class Field extends Rectangle {
+
+    Rectangle goal;
+    Rectangle player;
+    Rectangle opponent;
+    Line midField;
+
+    public Field() {
+    }
+}
